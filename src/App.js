1c829212b6d680849eeb7bd9a91958c540c42c2b
avinash84319 <avichinnu22@gmail.com>
2023-12-29 00:13:17 +0530

    login page done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import React from 'react';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import Register from './screens/Register';
+import Otp from './screens/Otp';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/otp" element={<Otp />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
